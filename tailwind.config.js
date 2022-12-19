924e75e12072159343aa5d52ce57d4cad363daf9
Jonatas Queiroz Lima <joqlima5@gmail.com>
2022-12-19 05:23:35 -0300

    feat: :wrench: add orange theme color in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Helvetica Neue', 'Helvetica, Arial', 'sans-serif']
+      },
+      colors: {
+        orangetheme: {
+          normal: '#FF6C00',
+          hover: '#D45A00'
+        }
       }
     }
   },
